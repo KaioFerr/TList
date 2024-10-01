3281ebc686884fe2800189db1a9198db725def1f
Kaio <kaiohtf38@gmail.com>
2024-10-01 13:46:37 -0300

    atualizando projeto

--- src/main/kotlin/br/com/TList/Controller.kt
+++ src/main/kotlin/br/com/TList/Controller.kt
@@ -1,30 +1,45 @@
 package br.com.TList
 
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
 @RestController
-class Controller {
+class Controller (
+    private val namedParameterJdbcOperations: NamedParameterJdbcOperations
+) {
 
     @GetMapping("/ping")
     fun ping(): String {
         return "pong";
     }
-    @GetMapping("/produtos")
-    fun produtos(): List<Product>{
-        /*val products = listOf(
-            Product(
-                id = UUID.randomUUID(),
-                name = "Produto 01",
-                description = "O produto é muito bom"
-            ),
-            Product(
-                id = UUID.randomUUID(),
-                name = "Produto 02",
-                description = "O produto é muito bom"
+    @GetMapping("/lista")
+    fun list(): List<User>{
+//        val toDoList = listOf(
+//            ToDoList(
+//                    id = UUID.randomUUID(),
+//            name = "Tarefa 01",
+//            description = "Ir ao mercado"
+//        ),
+//        ToDoList(
+//            id = UUID.randomUUID(),
+//            name = "Tarefa 02",
+//            description = "Estudar algoritmo"
+//        )
+//        )
+
+        val users = namedParameterJdbcOperations.query("select id, username, email, password from users", {
+            rs, rows ->
+            User(
+                id = UUID.fromString(rs.getString("id")),
+                username = rs.getString("username"),
+                email = rs.getString("email"),
+                password = rs.getString("password")
+
             )
-        )
-        return products*/
+        })
+
+        return users
     }
 }
---
+++ src/main/kotlin/br/com/TList/User.kt
@@ -0,0 +1,11 @@
+package br.com.TList
+
+import java.util.UUID
+
+class User(
+    val id: UUID,
+    val username: String,
+    val email: String,
+    val password: String
+
+)
